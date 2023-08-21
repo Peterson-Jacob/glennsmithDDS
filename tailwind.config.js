2672751194c45a6fb354fb5f6c000b3426fa6f76
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2023-08-21 14:58:41 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
 
       fontFamily: {
         'Cederville':[' "Cedarville Cursive", cursive;']
+      },
+
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
       }
       
     },
